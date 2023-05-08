3db49a8057062af3f9dc19be34aaa528d4f4aac9
Taghrid(Teto) <88310497+T20T@users.noreply.github.com>
2023-05-08 09:52:44 +0300

    Add files via upload

---
+++ Sun/Constantsand Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+/*:
+## Exercise - Variables
+ 
+ Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
+ */
+var Schooling = 9
+print(Schooling)
+
+//:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
+
+var x = 1
+print(Schooling + x)
+
+//:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
+print("at first no becuse we can't declar 2 valuse to the same var so to fix it I declar other var and I calle it x ")
+
+/*:
+[Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
+ */
---
+++ Sun/Constantsand Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+/*:
+## App Exercise - Fitness Tracker: Constant or Variable?
+ 
+ >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ 
+ There are all sorts of things that a fitness tracking app needs to keep track of in order to display the right information to the user. Similar to the last exercise, declare either a constant or a variable for each of the following items, and assign each a sensible value. Be sure to use proper naming conventions.
+ 
+- Name: The user's name
+- Age: The user's age
+- Number of steps taken today: The number of steps that a user has taken today
+- Goal number of steps: The user's goal for number of steps to take each day
+- Average heart rate: The user's average heart rate over the last 24 hours
+ */
+var UName = "Taghrid" // the app used by many useres so the name is var
+var UAge: Int;"27" // the age is var debend on diffrent user's age
+var StepsCount = "3000" // this is the counter of steps during the dya which increas by time so its var
+let GoalNumber = "5000" // this is set goal of the user want to reach every day so its let(constant) but** as user I can change my goal later so if the app has this featuer I may use var to set my goal again then it will be let or maybe I declare changmygoal to var
+var AverageHR = "110" // Heart rates are varity from day to day and user to other so its variable. 
+/*:
+ Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
+ 
+[Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Types and Type Safety](@next)
+ */
